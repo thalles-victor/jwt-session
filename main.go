6c9bfbc8ad9cd97a7e17aaf3b52dac767511fce7
thalles-victor <thalles.victor2012@gmail.com>
2025-10-19 12:48:15 -0300

    feat: create api doc.

--- main.go
+++ main.go
@@ -14,11 +14,14 @@ import (
 func main() {
 	logger.Init()
 	config.LoadEnv()
+
 	app := fiber.New()
+
+	// 🔹 Servir a documentação HTML diretamente no endpoint /
+	app.Static("/", "./api-doc") // <-- serve index.html da pasta api-doc
 
 	logger.Info.Println("connecting in the database")
 	db, err := database.Connect()
-
 	if err != nil {
 		logger.Error.Printf("error when connect in the database. error %s \n", err.Error())
 		log.Fatalf("error when connect in the database %s", err.Error())
@@ -32,5 +35,5 @@ func main() {
 
 	port := ":8080"
 	fmt.Printf("Start the server in http://localhost%s \n", port)
-	log.Fatal((app.Listen(port)))
+	log.Fatal(app.Listen(port))
 }
